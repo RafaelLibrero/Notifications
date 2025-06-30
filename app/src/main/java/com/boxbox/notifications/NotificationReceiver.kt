efe82a76e85a3d8195f44d7dc3a026d0628e7d22
RafaelLibrero <librerorafael@outlook.com>
2025-06-30 19:27:46 +0200

    Notification with action

--- app/src/main/java/com/boxbox/notifications/NotificationReceiver.kt
+++ app/src/main/java/com/boxbox/notifications/NotificationReceiver.kt
@@ -5,12 +5,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.util.Log
 import android.widget.Toast
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val message = intent.getStringExtra("message") ?: "Recordatorio"
+
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
 
         Toast.makeText(context, "Alarma recibida: $message", Toast.LENGTH_SHORT).show()
         Log.d("NotificationReceiver", "Alarma recibida: $message")
@@ -21,6 +25,7 @@ class NotificationReceiver : BroadcastReceiver() {
             .setContentTitle("Recordatorio")
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .build()
 
